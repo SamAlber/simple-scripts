9d10b15e1fdcfee07e0e63456503ebf816389435
Samuel Albershtein <sam.albershtein@gmail.com>
2024-07-24 05:10:31 +0300

    Adding comments to the code

--- simple_extraction.py
+++ simple_extraction.py
@@ -3,31 +3,35 @@
 """
 SAMUEL ALBERSHTEIN - FIRST LINE TEXT EXTRACTION
 
-This script prompts the user for a file path and extracts the first line of the specified file.
+This script prompts the user for a file path and extracts the first line of the file.
+I also added a path check with the OS module to analyze annoying errors :) 
 """
 
-import os 
+import os # Importing the os module to check if path exists 
 
-def get_first_line(path):
+def get_first_line(path): # A function that receives the path, 'with open' gets the file located in this path and opens it in read! 
+
     with open(path, 'r') as file:
         first_line = file.readline().strip()
+
     return first_line
 
 
-while True:
 
-    path = input("Please enter the path to the log file (q for quit): ")
+while True: # Iterating endlessly until we decide to stop by pressing q and break
 
-    if path.lower() == 'q':
-        break
+    path = input("Please enter the path to the log file (q for quit): ") # Don't forget to add the extension of file if needed (.txt)
 
-    if os.path.exists(path):
-        try:
+    if path.lower() == 'q': 
+        break 
+
+    if os.path.exists(path): # Will help us see if there are errors or permission problems with the path 
+        try: 
             print(f"This path exists\nThe first line of the log is: {get_first_line(path)}")
-        except Exception as e:
+        except Exception as e: # Will catch any error and show it for us for analysis 
             print(f"{e}")
     else:
-        print("Wrong path, please try again")
+        print("Wrong path, please try again") 
         
         
 
